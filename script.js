1b4e7270e7f9eadefac5e46279849308b9362d81
Fuad2005 <fuadqasimov000@gmail.com>
2022-11-23 22:29:17 +0400

    home almost finished, about started

--- script.js
+++ script.js
@@ -1,59 +1,80 @@
-const checkbox = document.getElementById("ChangeTheme");
-const links = document.getElementsByTagName("a");
-const header = document.querySelector("header");
-const h1 = document.getElementsByTagName("h1");
-const p = document.getElementsByTagName("p");
+// const checkbox = document.getElementById("ChangeTheme");
+// const links = document.getElementsByTagName("a");
+// const header = document.querySelector("header");
+// const h1 = document.getElementsByTagName("h1");
+// const p = document.getElementsByTagName("p");
+// const small = document.getElementsByTagName("small");
 
 
-if (sessionStorage.getItem("mode") == "dark") {
-  darkmode();
-} else {
-  nodark();
-}
+// if (sessionStorage.getItem("mode") == "dark") {
+//   darkmode();
+// } else {
+//   nodark();
+// }
 
-checkbox.addEventListener("click", function() {
-  if (checkbox.checked) {
-      nodark();
-} else {
-      darkmode();
-  }
-});
+// checkbox.addEventListener("click", function() {
+//   if (checkbox.checked) {
+//       nodark();
+// } else {
+//       darkmode();
+//   }
+// });
 
-function darkmode() {
-  document.body.classList.add("dark-mode-bg"); 
-  header.style.backgroundColor = "#292E33";
-  checkbox.checked = false; 
-  sessionStorage.setItem("mode", "dark"); 
-  for(let i = 0; i < links.length; i++){
-    links[i].style.color = "#fff";
-    }
-    for(let i = 0; i < h1.length; i++){
-    h1[i].style.color = "#fff";
-    }
-    for(let i = 0; i < p.length; i++){
-    p[i].style.color = "#fff";
-    }
-}
-
-function nodark() {
-  document.body.classList.remove("dark-mode-bg"); 
-  header.style.backgroundColor = "#fff";
-  checkbox.checked = true; 
-  sessionStorage.setItem("mode", "light"); 
-  for(let i = 0; i < links.length; i++){
-    links[i].style.color = "#292E33";
-
-}
-for(let i = 0; i < h1.length; i++){
-    h1[i].style.color = "#292E33";
-    }
-    for(let i = 0; i < p.length; i++){
-    p[i].style.color = "#292E33";
-    }
+// function darkmode() {
+//     document.body.classList.remove("light-mode-bg"); 
+//   document.body.classList.add("dark-mode-bg"); 
+//   header.style.backgroundColor = "#181A1B";
+//   checkbox.checked = false; 
+//   sessionStorage.setItem("mode", "dark"); 
+//   for(let i = 0; i < links.length; i++){
+//     links[i].style.color = "#fff";
+//     }
+//     for(let i = 0; i < h1.length; i++){
+//     h1[i].style.color = "#fff";
+//     }
+//     for(let i = 0; i < p.length; i++){
+//     p[i].style.color = "#fff";
+//     }
+//     for(let i = 0; i < small.length; i++){
+//         small[i].style.color = "#a2ff00e2";
+//         }
     
+// }
+
+// function nodark() {
+//   document.body.classList.remove("dark-mode-bg");
+//   document.body.classList.add("light-mode-bg"); 
+//   header.style.backgroundColor = "#fff";
+//   checkbox.checked = true; 
+//   sessionStorage.setItem("mode", "light"); 
+//   for(let i = 0; i < links.length; i++){
+//     links[i].style.color = "#181A1B";
+
+// }
+//     for(let i = 0; i < h1.length; i++){
+//     h1[i].style.color = "#181A1B";
+//     }
+//     for(let i = 0; i < p.length; i++){
+//     p[i].style.color = "#181A1B";
+//     }
+//     for(let i = 0; i < small.length; i++){
+//         small[i].style.color = "#12A708";
+//         }
+
+// }
+
+
+
+
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 60);
 }
+)
 
 
 
 
-console.log(h1)
+
+
